c6b62c3176cd06b3d62d7013ff7544176fcf957c
Pavel Vasilev <p.n.vasilev@gmail.com>
2023-01-22 22:38:41 +0100

    Auto-convert class to Kotlin.

--- src/main/java/ch/study/Person.kt
+++ src/main/java/ch/study/Person.kt
@@ -1,20 +1,3 @@
-package ch.study;
+package ch.study
 
-public class Person {
-
-    private final String name;
-    private final int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-}
+class Person(val name: String, val age: Int)
